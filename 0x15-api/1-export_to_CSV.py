dba7b0970459fc1d4c12a554a651c9305ebdddb3
Krofah <amponsahkrofah@gmail.com>
2023-07-27 23:55:11 +0000

    task 0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,59 @@
 #!/usr/bin/python3
-"""Python Script to export data in CSV format"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
-from requests import get
+import requests
 from sys import argv
 
 
-def exportCsv(user):
-        data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-                user)).json()
-                    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
-                            user)).json().get('username')
-                                employee_data = open('{}.csv'.format(user), 'w')
-                                    cwrite = csv.writer(employee_data, quoting=csv.QUOTE_ALL)
-                                        for line in data:
-                                                lined = [line.get('userId'), name,
-                                                                 line.get('completed'), line.get('title')]
-                                                                         cwrite.writerow(lined)
-                                                                             employee_data.close()
+def to_csv():
+    """Export API data to CSV"""
+    if len(argv) < 2:
+        print("Error: Employee ID not provided.")
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        return
+
+    try:
+        employee_id = int(argv[1])
+    except ValueError:
+        print("Error: Invalid employee ID. Please provide a valid integer.")
+        return
+
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    if users.status_code != 200:
+        print("Error: Failed to retrieve user data from the API.")
+        return
+
+    for user in users.json():
+        if user.get('id') == employee_id:
+            username = user.get('username')
+            break
+    else:
+        print(f"Error: User with ID {employee_id} not found.")
+        return
+
+    task_status_title = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    if todos.status_code != 200:
+        print("Error: Failed to retrieve task data from the API.")
+        return
+
+    for task in todos.json():
+        if task.get('userId') == employee_id:
+            task_status_title.append((task.get('completed'), task.get('title')))
+
+    filename = f"{employee_id}.csv"
+    with open(filename, "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        for task in task_status_title:
+            writer.writerow({
+                "USER_ID": employee_id,
+                "USERNAME": username,
+                "TASK_COMPLETED_STATUS": task[0],
+                "TASK_TITLE": task[1]
+            })
 
 
-                                                                             if __name__ == "__main__":
-                                                                                 exportCsv(argv[1])
+if __name__ == "__main__":
+    to_csv()
